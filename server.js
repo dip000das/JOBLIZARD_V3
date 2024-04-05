194aa5ab8685e25a3f31ef0db26a0db707ff7b3e
Dip <dipkumardas2002@gmail.com>
2024-04-05 09:55:51 +0530

    server updated

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ import cloudinary from "cloudinary";
 // require('dotenv').config()
 import dotenv from 'dotenv';
 dotenv.config();
-
+const PORT= process.env.PORT||4000
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
   api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running at port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
 });
